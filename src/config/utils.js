8c7f90b054a48454104cb02c092cf2bff5d06a9a
David Celis <78105160+Schied@users.noreply.github.com>
2021-10-13 11:59:50 -0500

    Delete

--- src/config/utils.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    DB: process.env.DB || 'mongodb+srv://Schied:8020457@orraivsplay.8hncu.mongodb.net/ApiRest?retryWrites=true&w=majority'
-}
